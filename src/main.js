84886b245db38654ca9e8bad935d75f42823d5d0
ming <jsps595214@gmail.com>
2020-03-30 16:59:19 +0800

    價金開工

--- src/main.js
+++ src/main.js
@@ -1,7 +1,40 @@
+//  eslint absolute path should set before relative path
 import Vue from 'vue';
+import 'bootstrap';
+import $ from 'jquery';
+// vue type plugin
+import VueTyperPlugin from 'vue-typer';
+// vee validate
+import { ValidationProvider, extend } from 'vee-validate';
+import { required, email } from 'vee-validate/dist/rules';
+import { messages } from 'vee-validate/dist/locale/zh_TW.json';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+window.$ = $;
+// vue type plugin
+Vue.use(VueTyperPlugin);
+// vee validate
+Vue.component('ValidationProvider', ValidationProvider);
+extend('email', {
+  ...email,
+  message: messages.email,
+});
+// Override the default message.
+extend('required', {
+  ...required,
+  message: messages.required,
+});
+extend('minmax', {
+  validate: (value, args) => {
+    const { length } = value;
+    return length >= args.min && length <= args.max;
+  },
+  params: ['min', 'max'],
+  message: '有字數限制',
+});
+
 
 Vue.config.productionTip = false;
 
